452e4c9f3442f24e88c8481a1e805f66ec21f04d
Jason Judkins <34417573+jcjudkins@users.noreply.github.com>
2021-10-11 02:12:35 -0400

    fixing JS

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-let display = document.getElementById('display');
-let buttons = document.getElementById('myButton')
+let display = document.getElementById('display')
+let clear = document.getElementById('clear')
+let buttons = document.querySelectorAll('mybutton')
+let equals = document.getElementById('equals')
 
-
-for (let button of buttons){
+for (let button of buttons) {
     button.addEventListener('click', () => {
-        display.innerText += button.innerText;
+        display.innerText += button.innerText
     })
-    }
-// let equals = document.getElementById('equals')
-//     equals.addEventListener('click',)
+}
+
+let equals = document.getElementById('equals')
+let multiply = document.getElementById('multiply')
+let divide = document.getElementById('divide')
+let subtract = document.getElementById('subtract')
+let decimal = document .getElementById('decimal')
+
+equals.addEventListener('click', () => {
+   display.innerText = eval(display.innerText)
+})
+
+clear.addEventListener('click', () => {
+    location.reload()
+ })
+
+decimal.addEventListener('click', () => {
+        display.innerText += decimal.innerText
+      })
 
